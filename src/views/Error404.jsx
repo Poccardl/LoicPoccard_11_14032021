f77bb0f78035d231e5a690423965820826ac6deb
Poccardl <loic.poccard@gmail.com>
2021-03-17 01:02:15 +0100

    add component -> Error404 / some updates

--- src/views/Error404.jsx
+++ src/views/Error404.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+
 
 export class Error404 extends React.Component {
 
     render () {
+        console.log(this.props)
         return (
-            <>404 ERROR</>
+            <>
+            <div className="error404">
+                <h1>404</h1>
+                <p>Oups! La page que vous demandez n'existe pas.</p>
+                <div className="mb_100">
+                    <Link to="/Home" className="a">Retourner sur la page d'accueil</Link>
+                </div>
+            </div>
+            </>
             )
     }
 }
